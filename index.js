d7f77d6095a0b6de8d473f02e2aeb669661d5acc
ashik <mdashikash18@gmail.com>
2024-10-10 09:35:28 +0000

    Solution

---
+++ index.js
@@ -0,0 +1,21 @@
+// import 'bootstrap/dist/css/bootstrap.min.css'
+
+const Button = props => {
+  //  Write your code here.
+  const {name, classBtn} = props
+  return <button className={`${classBtn}`}>{name}</button>
+}
+
+const element = (
+  //  Write your code here.
+  <div>
+    <h1 className='heading'>Social Buttons</h1>
+    <div>
+      <Button name='Like' classBtn='button like' />
+      <Button name='Comment' classBtn='button comment' />
+      <Button name='Share' classBtn='button share' />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
